c5d908c41295b158fa519161e117dfb92b345ad6
Dustin Key <Keydustin90@gmail.com>
2022-07-12 17:56:43 -0500

    done

---
+++ FactoryPattern/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+namespace FactoryPattern
+{
+    public class ConsoleLogger
+    {
+        public static void VehicleStartUpDialogue()
+        {
+            Console.WriteLine("Put Key ignition");
+            Thread.Sleep(1000);
+            Console.WriteLine("Turn Key!");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int wheeCount;
+            bool input;
+            {
+                Console.WriteLine("Enter the amount of tries for the vehicl you want to create:");
+
+                input = int.TryParse(Console.ReadLine(), out wheeCount);
+
+            }while (input == false);
+            _ = VehicleFactory.GetVehicle(wheeCount);
+
+        }
+    }
+}
